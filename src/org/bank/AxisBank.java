359d1b0b6bdc82c6640aa496e6159de7c4c9b51c
SaravananRaja12 <sara12111997@gmail.com>
2022-09-15 15:45:12 +0530

    Updated B

--- src/org/bank/AxisBank.java
+++ src/org/bank/AxisBank.java
@@ -7,4 +7,10 @@ public class AxisBank {
 	public void savings() {
 		System.out.println("savings AC  2 ");
 	}
+	public void deposit11() {
+		System.out.println("Deposit AC  22 ");
+	}
+	public void savings22() {
+		System.out.println("savings AC  332 ");
+	}
 }
